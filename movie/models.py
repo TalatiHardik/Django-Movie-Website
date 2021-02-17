8e9645a0d3451628d4a80defa456eb929a812fb7
TalatiHardik <hardikhtalati@gmail.com>
2021-02-17 14:25:54 +0530

    Added : Nav bar urls fixed and models edited

--- movie/models.py
+++ movie/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 CATEGORY_CHOICES = (
-    ('A','ACTION'),
-    ('D','DRAMA'),
-    ('C','COMEDY'),
-    ('R','ROMANCE')
+    ('action','ACTION'),
+    ('drama','DRAMA'),
+    ('comdey','COMEDY'),
+    ('romance','ROMANCE')
 
 )
 
@@ -26,7 +26,7 @@ class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
     image = models.ImageField(upload_to='movies')
-    category = models.CharField(choices=CATEGORY_CHOICES , max_length=1)
+    category = models.CharField(choices=CATEGORY_CHOICES , max_length=10)
     langugage = models.CharField(choices=LANGUAGE_CHOICES , max_length=2)
     status = models.CharField(choices=STATUS_CHOICES , max_length=2)
     year_of_production = models.DateField()
--- movie/urls.py
+++ movie/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import MovieDetail , MovieList
+from .views import MovieDetail , MovieList , MovieCategory
 
+app_name = 'movie'
 
 urlpatterns = [
     path('<int:pk>', MovieDetail.as_view() , name='movie_detail'),
     path('', MovieList.as_view() , name='movie_list'),
+    path('category/<str:category>', MovieCategory.as_view() , name='movie_category'),
 ]
