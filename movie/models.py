7839680ef66bc2a8e384181ac7bfe39a5eb7fe0b
TalatiHardik <hardikhtalati@gmail.com>
2021-02-17 12:27:15 +0530

    Changed: added moviedetail model and changed the html files respec

--- movie/models.py
+++ movie/models.py
@@ -31,6 +31,21 @@ class Movie(models.Model):
     status = models.CharField(choices=STATUS_CHOICES , max_length=2)
     year_of_production = models.DateField()
     views_count = models.IntegerField(default=0)
+    cast = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
+LINK_CHOICES = (
+    ('D','DOWNLOAD LINK'),
+    ('W','WATCH LINK'),
+
+)
+
+class MovieLinks(models.Model):
+    movie = models.ForeignKey(Movie, related_name='movie_watch_link' ,on_delete=models.CASCADE)
+    type = models.CharField(choices=LINK_CHOICES , max_length = 1)
+    link = models.URLField()
+
+    def __str__(self):
+        return str(self.movie)+" "+str(self.type)
+    
--- movie/views.py
+++ movie/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView , DetailView
-from .models import Movie
+from .models import Movie , MovieLinks
 
 class MovieList(ListView):
     model = Movie
     paginate_by = 1
 class MovieDetail(DetailView):
     model = Movie
+
+    def get_object(self):
+        object = super(MovieDetail,self).get_object()
+        object.views_count += 1
+        object.save()
+        return object
+    def get_context_data(self , **kwargs):
+        context = super(MovieDetail,self).get_context_data(**kwargs)
+        context['links'] = MovieLinks.objects.filter(movie=self.get_object())
+        return context
+    
