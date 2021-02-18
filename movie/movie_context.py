d68c4dfe459c368c359d75e96df898bd5eb317f3
TalatiHardik <hardikhtalati@gmail.com>
2021-02-18 09:43:41 +0530

    Added: Home page added , Fixed: Slider fixed

--- movie/movie_context.py
+++ movie/movie_context.py
@@ -1,5 +1,5 @@
 from .models import Movie
 
 def slider_movies(request):
-    movies = Movie.objects.all().order_by('created')[0:4]
+    movies = Movie.objects.all().order_by('created')[0:3]
     return {'slider_movies' : movies }
--- movie/views.py
+++ movie/views.py
@@ -5,9 +5,24 @@ from django.views.generic import ListView , DetailView
 from django.views.generic.dates import YearArchiveView
 from .models import Movie , MovieLinks
 
+
+class HomeView(ListView):
+    model = Movie
+    template_name = 'movie/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView , self).get_context_data(**kwargs)
+        context['top_rated'] = Movie.objects.filter(status='TR')
+        context['most_watched'] = Movie.objects.filter(status='MW')
+        context['recently_added'] = Movie.objects.filter(status='RA')
+        return context
+
+   
+
 class MovieList(ListView):
     model = Movie
     paginate_by = 2
+
 class MovieDetail(DetailView):
     model = Movie
 
@@ -19,8 +34,9 @@ class MovieDetail(DetailView):
     def get_context_data(self , **kwargs):
         context = super(MovieDetail,self).get_context_data(**kwargs)
         context['links'] = MovieLinks.objects.filter(movie=self.get_object())
+        context['related_movies'] = Movie.objects.filter(category=self.get_object().category)#.order_by['created'][0:6]
         return context
-    
+
 class MovieCategory(ListView):
     model = Movie
     paginate_by = 2
